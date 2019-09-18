1d0da204e9f2b245ee800e44348042bd2de152ea
lucas <lucas_buttow@outlook.com>
2019-09-17 22:34:52 -0300

    configuração e testes nos caminhos do sistema

--- Views/template.blade.php
+++ Views/template.blade.php
@@ -7,45 +7,45 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="http://localhost/projetocomun/assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://projetocomun/assets/css/style.css">
 </head>
 <body>
 
 	<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 		<div class="container">
-		<a class="navbar-brand" href="#main">Comun.</a>
-		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarResponsive">
-			<ul class="navbar-nav ml-auto">
-			<?php if( !isset($_SESSION['login']) ){ ?>
-			<li class="nav-item">
-				<a class="nav-link" href="#about" id="scrollSuave">Sobre</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#formulario" data-toggle="modal" data-target="#exampleModalCenter">Contato</a>
-			</li>
-			
-			<li class="nav-item">
-				<a class="nav-link" data-toggle="modal" data-target="#userModal">Área do Cliente</a>
-			</li>                    
+			<a class="navbar-brand" href="#main">Comun.</a>
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+				<?php if( !isset($_SESSION['login']) ){ ?>
+				<li class="nav-item">
+					<a class="nav-link" href="#about" id="scrollSuave">Sobre</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#formulario" data-toggle="modal" data-target="#exampleModalCenter">Contato</a>
+				</li>
+				
+				<li class="nav-item">
+					<a class="nav-link" data-toggle="modal" data-target="#userModal">Área do Cliente</a>
+				</li>                    
 
-			<li class="nav-item">
-				<a class="nav-link" data-toggle="modal" data-target="#adminModal">Administrador</a>
-			</li>
-			<?php
-			}
-			else {
-			?>
-			<li class="nav-item">
-				<a class="nav-link" href="http://localhost/projetocomun/login/logout" id="scrollSuave">Logout</a>
-			</li>
-			<?php	
-			} 
-			?>
-			</ul>
-		</div>
+				<li class="nav-item">
+					<a class="nav-link" data-toggle="modal" data-target="#adminModal">Administrador</a>
+				</li>
+				<?php
+				}
+				else {
+				?>
+				<li class="nav-item">
+					<a class="nav-link" href="https://projetocomun/login/logout" id="scrollSuave">Logout</a>
+				</li>
+				<?php	
+				} 
+				?>
+				</ul>
+			</div>
 		</div>
 	</nav>   	
 
@@ -57,6 +57,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/additional-methods.js"></script>
-<script type="text/javascript" src="http://localhost/projetocomun/assets/js/script.js"></script>	
+<script type="text/javascript" src="https://projetocomun/assets/js/script.js"></script>	
 </body>
 </html>
