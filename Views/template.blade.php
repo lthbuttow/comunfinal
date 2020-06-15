8f8c84d27fc6fad0ca466801a23e544c25fcd85d
lucasbuttow <lucas_buttow@outlook.com>
2020-06-15 18:22:38 -0300

    ajustando acao de clique no logo COMUN

--- Views/template.blade.php
+++ Views/template.blade.php
@@ -13,7 +13,16 @@
 
 	<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 		<div class="container">
-		<a class="navbar-brand" href="#main">Comun.</a>
+		<?php if( isset($_SESSION['admin']) ){ ?>	
+			<a class="navbar-brand" href="http://localhost:8888/projetocomun/admin">Comun.</a>
+		<?php
+		}
+		else {
+		?>
+			<a class="navbar-brand" href="http://localhost:8888/projetocomun/usuario">Comun.</a>
+		<?php	
+		} 
+		?>
 		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
